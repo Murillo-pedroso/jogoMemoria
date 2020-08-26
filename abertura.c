3cebb15e35e1c853d8e481e3ec39afcc3cf5d248
Murillo-pedroso <43767213+Murillo-pedroso@users.noreply.github.com>
2020-08-26 11:31:17 -0300

    Add files via upload
    abertura colorida

---
+++ abertura.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctype.h>
+#include <Windows.h>
+
+int main(){
+    char teste, teste2, teste3;
+    teste2 = 220;
+    teste = 178;
+    teste3 = 223;
+//JOGO-start
+    //linha 1
+    system("cls");
+    system("color 01");
+    printf("\t\t|---------------------------------------------------------------|\n");
+    printf("\t\t|\t\t");
+    printf("%c%c%c%c%c%c   %c%c%c     %c%c%c%c    %c%c%c ", teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2);
+    printf("\t\t\t|\n");
+    Sleep(150);
+    //linha 2
+    system("color 02");
+    printf("\t\t|\t\t");
+    printf("    %c   %c   %c   %c       %c   %c", teste, teste, teste, teste, teste, teste);
+    printf("\t\t\t|\n");
+    Sleep(150);
+    //linha 3
+    system("color 05");
+    printf("\t\t|\t\t");
+    printf("%c   %c   %c   %c   %c %c%c%c   %c   %c", teste2, teste, teste, teste, teste, teste2, teste2, teste2, teste, teste);
+    printf("\t\t\t|\n");
+    Sleep(150);
+    //linha 4
+    system("color 09");
+    printf("\t\t|\t\t");
+    printf("%c   %c   %c   %c   %c   %c   %c   %c", teste, teste, teste, teste, teste, teste, teste, teste);
+    printf("\t\t\t|\n");
+    Sleep(150);
+    //linha 5
+    system("color 0D");
+    printf("\t\t|\t\t");
+    printf(" %c%c%c     %c%c%c     %c%c%c     %c%c%c", teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3);
+    printf("\t\t\t|\n");
+    Sleep(150);
+    //JOGO-end
+
+    //DA-start
+    //linha 1
+    system("color 01");
+    printf("\t\t|\t\t\t");
+    printf("%c%c%c       %c%c%c", teste2, teste2, teste2, teste2, teste2, teste2);
+    printf("\t\t\t\t|\n");
+    Sleep(150);
+    //linha 2
+    system("color 02");
+    printf("\t\t|\t\t\t");
+    printf("%c  %c%c    %c   %c", teste, teste3, teste, teste, teste);
+    printf("\t\t\t\t|\n");
+    Sleep(150);
+    //linha 3
+    system("color 05");
+    printf("\t\t|\t\t\t");
+    printf("%c    %c   %c%c%c%c%c", teste, teste, teste, teste2, teste2, teste2, teste);
+    printf("\t\t\t\t|\n");
+    Sleep(150);
+    //linha 4
+    system("color 09");
+    printf("\t\t|\t\t\t");
+    printf("%c  %c%c    %c   %c", teste, teste2, teste, teste, teste);
+    printf("\t\t\t\t|\n");
+    Sleep(150);
+    //linha 5
+    system("color 0D");
+    printf("\t\t|\t\t\t");
+    printf("%c%c%c      %c   %c", teste3, teste3, teste3, teste3, teste3, teste3, teste3);
+    printf("\t\t\t\t|\n");
+    Sleep(150);
+    //DA-end
+
+    //MEMORIA-start
+    //linha 1
+    system("color 01");
+    printf("\t\t|\t");
+    printf("%c   %c   %c%c%c%c%c   %c   %c    %c%c%c      %c%c%c     %c    %c%c%c", teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2, teste2);
+    printf("\t|\n");
+    Sleep(150);
+    //linha 2
+    system("color 02");
+    printf("\t\t|\t");
+    printf("%c%c %c%c   %c       %c%c %c%c   %c   %c    %c   %c    %c   %c   %c", teste, teste2, teste2, teste, teste, teste, teste2, teste2, teste, teste, teste, teste, teste, teste3, teste, teste);
+    printf("\t|\n");
+    Sleep(150);
+    //linha 3
+    system("color 05");
+    printf("\t\t|\t");
+    printf("%c %c %c   %c%c%c%c    %c %c %c   %c   %c    %c%c%c%c     %c   %c%c%c%c%c", teste, teste, teste, teste, teste2, teste2, teste2, teste, teste, teste, teste, teste, teste, teste3, teste, teste3, teste, teste, teste2, teste2, teste2, teste);
+    printf("\t|\n");
+    Sleep(150);
+    //linha 4
+    system("color 09");
+    printf("\t\t|\t");
+    printf("%c   %c   %c       %c   %c   %c   %c    %c  %c     %c   %c   %c", teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste);
+    printf("\t|\n");
+    Sleep(150);
+    //linha 5
+    system("color 0D");
+    printf("\t\t|\t");
+    printf("%c   %c   %c%c%c%c%c   %c   %c    %c%c%c     %c   %c    %c   %c   %c", teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3, teste3);
+    printf("\t|\n");
+    printf("\t\t|---------------------------------------------------------------|\n");
+    Sleep(150);
+    //MEMORIA-end
+
+    system("color 0E");
+    Sleep(150);
+
+    system("color 04");
+    Sleep(150);
+
+    system("color 0D");
+    Sleep(150);
+
+    system("color 0F");
+    Sleep(150);
+
+    system("color 0D");
+    Sleep(150);
+
+    system("color 0F");
+    Sleep(150);
+
+    system("color 0D");
+    Sleep(150);
+
+    system("color 0D");
+    Sleep(150);
+
+    system("color 0F");
+    Sleep(150);
+
+    system("color 0D");
+    Sleep(150);
+
+    system("color 0F");
+    Sleep(150);
+
+    printf("\n\n\t\t\t  ");
+    system("pause");
+
+    return 0;
+}
